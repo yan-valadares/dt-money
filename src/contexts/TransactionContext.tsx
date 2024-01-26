2b140c5d35a8df49e5faca596779e5adea041b72
yancardoso <yancardoso@outlook.com.br>
2024-01-26 15:05:33 -0300

    feature: add filter to transaction search, also change json-server version

--- src/contexts/TransactionContext.tsx
+++ src/contexts/TransactionContext.tsx
@@ -1,4 +1,5 @@
 import { ReactNode, createContext, useEffect, useState } from "react";
+import { api } from "../lib/axios";
 
 interface Transactions {
     id: number,
@@ -11,6 +12,7 @@ interface Transactions {
 
 interface TransactionContextType {
     transactions: Transactions[]
+    fetchTransactions: (query?: string) => Promise<void>
 }
 
 interface TransactionsProviderProps {
@@ -22,19 +24,26 @@ export const TransactionsContext = createContext({} as TransactionContextType)
 export function TransactionsProvider({children}: TransactionsProviderProps){
     const [transactions, setTransactions] = useState<Transactions[]>([])
     
-    async function loadTransactions() {
-        const response = await fetch('http://localhost:3000/transactions')
-        const data = await response.json()
+    async function fetchTransactions(query?: string) {
 
-        setTransactions(data)
+        const response = await api.get('/transactions', {
+            params: {
+                q: query
+            }
+        })
+
+        setTransactions(response.data)
     }
     
     useEffect(() => {
-        loadTransactions()
+        fetchTransactions()
     }, [])
 
     return (
-        <TransactionsContext.Provider value={{ transactions}}>
+        <TransactionsContext.Provider value={{ 
+            transactions,
+            fetchTransactions
+        }}>
             {children}
         </TransactionsContext.Provider>
     )
